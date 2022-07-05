5ef34cbd363b814bd9989a16e8a725461d828aa8
STI Desarrollador2 <desarrollador2@robertodelrio.local>
2022-07-05 16:52:19 -0400

    casi

--- application/views/gestorDocumentos.php
+++ application/views/gestorDocumentos.php
@@ -5,17 +5,75 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <!--BOTONES BUSCAR & CREAR NUEVO DOCUMENTO-->
 <div class="container-sm">
     <div class="row justify-content-center p-4">
-            <button id="buscarDocumentos" class="col col-5 btn btn-primary border border-3 bg-primary rounded-3 text-white" style="--bs-bg-opacity: .80;">
-                <i class="fab fa-searchengin"></i> Buscar Documentos
-            </button>
-
-            <button id="crearDocumento" class="col col-5 btn btn-success border border-3 bg-success rounded-3 text-white" style="--bs-bg-opacity: .80;">
-                <i class="fas fa-envelope-open-text"></i> Crear Nuevo Documento
-            </button>
+        <button id="buscarDocumentos" class="col col-5 btn btn-primary border border-3 bg-primary rounded-3 text-white" style="--bs-bg-opacity: .80;">
+            <i class="fab fa-searchengin p-2"></i> Buscar Documentos
+        </button>
+       
+        <button id="crearDocumento" class="col col-5 btn btn-success border border-3 bg-success rounded-3 text-white" style="--bs-bg-opacity: .80;">
+            <i class="fas fa-envelope-open-text p-2"></i> Crear Nuevo Documento
+        </button>
     </div>
 </div>
 
 <!--TABLA QUE CONTIENE DATOS DE BDO -->
-<div class="container-sm">
-
+<div class="container-sm w-50">
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="list-group list-group-horizontal" id="list-tab" role="tablist">
+                <a class="list-group-item list-group-item-action border border-2" id="lPendientes" data-bs-toggle="list" href="#list-pendientes">Pendientes</a>
+                <a class="list-group-item list-group-item-action border border-2" id="lRecibidos" data-bs-toggle="list" href="#list-recibidos" role="tab">Recibidos</a>
+                <a class="list-group-item list-group-item-action border border-2" id="lEnviados" data-bs-toggle="list" href="#list-enviados" role="tab">Enviados</a>
+                <a class="list-group-item list-group-item-action border border-2" id="lRbusqueda" data-bs-toggle="list" href="#list-rbusqueda" role="tab">Resultado Búsqueda</a>
+            </div>
+        </div>
+        
+        <div class="row justify-content-center">
+            <div class="col">
+                <div class="tab-content" id="nav-tabContent">
+                <div class="tab-pane fade" id="list-pendientes" role="tabpanel">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Color</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Turtle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">Blue</th>
+                                <td>Leonardo</td>
+                                <td>da Vinci</td>
+                                <td>Leo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Red</th>
+                                <td>Raffaello</td>
+                                <td>Sanzio da Urbino</td>
+                                <td>Raph</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Purple</th>
+                                <td>Donato</td>
+                                <td>di Niccolò di Betto Bardi</td>
+                                <td>Donnie</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Orange</th>
+                                <td>Michelangelo</td>
+                                <td>di Lodovico Buonarroti Simoni</td>
+                                <td>Mikey</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                    <div class="tab-pane fade" id="list-recibidos" role="tabpanel">recibidos</div>
+                    <div class="tab-pane fade" id="list-enviados" role="tabpanel">enviados</div>
+                    <div class="tab-pane fade" id="list-rbusqueda" role="tabpanel">resultado de la busqueda</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
--- application/views/template/cabecera.php
+++ application/views/template/cabecera.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--LINK CSS Y FONT AWESOME-->
+    <!--LINK CSS,FONT AWESOME-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <title>Gestor de Documentación</title>
@@ -18,21 +18,21 @@
         <nav class="navbar navbar-expand-sm border border-2 bg-primary rounded-2" style="--bs-bg-opacity: .80;">
             <div class="container-fluid">
                 <a class="navbar-brand text-white" href="#">
-                    <i class="far fa-folder"></i>
+                    <i class="far fa-folder p-2"></i>
                     Seguimiento de Documentación
                 </a>
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item navbar-text text-white">
                         <!--ICONOS EN ETIQUETA SPAM-->
                         <span>
-                            <i class="fas fa-hospital-user"></i>
+                            <i class="fas fa-hospital-user p-2"></i>
                             Usuario
                         </span>
                     </li>
                     <li class="nav-item active">
                         <span>
                             <a class="nav-link text-white" href="http://intranetdes">
-                                <i class="fas fa-undo"></i>
+                                <i class="fas fa-undo p-2"></i>
                                 Volver al Inicio</a>
                         </span>
                     </li>
