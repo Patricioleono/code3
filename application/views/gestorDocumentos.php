9893295231da1e2969ad90537d03cddcf0e639c2
STI Desarrollador2 <desarrollador2@robertodelrio.local>
2022-07-11 16:44:21 -0400

    sidebar arreglada sin terminar

--- application/views/gestorDocumentos.php
+++ application/views/gestorDocumentos.php
@@ -2,82 +2,146 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 ?>
 <!--CONTENEDOR SIDEBAR & DATA-TABLE -->
-
-<div class="row">
-    <div class="col col-3 mb-1 pt-1 ms-1 position-fixed" id="sticky-sidebar">
-        <div class="row">
-            <nav class="navbar navbar-expand-lg">
-                <div class="container row ms-2 col-12">
-                    <button class="btn buttonCustomCrear col col-9"> <i class="fa-solid fa-square-plus"></i> Crear Documento</button>
-                </div>
-            </nav>
-
-            <div class="ms-2 col col-9">
-                <div class="list-group" id="list-tab" role="tablist">
-                    <a class="list-group-item list-group-item-action text-center" id="list-recibidos-list" data-bs-toggle="list" href="#list-recibidos" role="tab" aria-controls="list-recibidos">
-                        Recibidos 
-                        <span class="badge text-bg-info">4</span>
-                    </a>
-                    <a class="list-group-item list-group-item-action text-center" id="list-pendientes-list" data-bs-toggle="list" href="#list-pendientes" role="tab" aria-controls="list-pendientes">
-                        Pendientes 
-                        <span class="badge colorYellow text-dark">4</span>
-                    </a>
-                    <a class="list-group-item list-group-item-action text-center" id="list-enviados-list" data-bs-toggle="list" href="#list-enviados" role="tab" aria-controls="list-enviados">
-                        Enviados 
-                        <span class="badge colorGreen text-dark">4</span>
-                    </a>
-                    <a class="list-group-item list-group-item-action text-center" id="list-aprobados-list" data-bs-toggle="list" href="#list-aprobados" role="tab" aria-controls="list-aprobados">
-                        Aprobados 
-                        <span class="badge colorBlue text-dark">4</span>
-                    </a>
-                    <a class="list-group-item list-group-item-active text-center" id="list-importantes-list" data-bs-toggle="list" href="#list-importantes" role="tab" aria-controls="list-importantes">
-                        Importantes 
-                        <span class="badge colorRed text-dark">4</span>
-                    </a>
-                </div>
-
-            </div>
+<div class="d-flex" id="wrapper">
+    <div class="bg-white border border-1" id="sidebar-wrapper">
+        <div class="sidebar-heading">
+            <button class="btn btn-outline-primary">
+                <i class="fa-solid fa-square-plus"></i>
+                Crear Documento
+            </button>
         </div>
 
+        <div class="list-group list-group-flush">
 
+            <a class="list-group-item" data-bs-toggle="list" href="#list-recibidos">
+                <i class="fa-solid fa-inbox"></i>
+                Recibidos
+                <span class="badge text-bg-info position-absolute end-0 me-5">4</span>
+            </a>
+
+            <a class="list-group-item text-star" id="list-pendientes-list" data-bs-toggle="list" href="#list-pendientes" role="tab" aria-controls="list-pendientes">
+                <i class="fa-solid fa-clock-rotate-left"></i>
+                Pendientes
+                <span class="badge colorYellow text-dark position-absolute end-0 me-5">4</span>
+            </a>
+
+            <a class="list-group-item text-star" id="list-enviados-list" data-bs-toggle="list" href="#list-enviados" role="tab" aria-controls="list-enviados">
+                <i class="fa-regular fa-paper-plane"></i>
+                Enviados
+                <span class="badge colorGreen text-dark position-absolute end-0 me-5">4</span>
+            </a>
+
+            <a class="list-group-item text-star" id="list-aprobados-list" data-bs-toggle="list" href="#list-aprobados" role="tab" aria-controls="list-aprobados">
+                <i class="fa-solid fa-envelope-circle-check"></i>
+                Aprobados
+                <span class="badge colorBlue text-dark position-absolute end-0 me-5">4</span>
+            </a>
+
+            <a class="list-group-item text-star" id="list-importantes-list" data-bs-toggle="list" href="#list-importantes" role="tab" aria-controls="list-importantes">
+                <i class="fa-solid fa-circle-exclamation"></i>
+                Importantes
+                <span class="badge colorRed text-dark position-absolute end-0 me-5">4</span>
+            </a>
+        </div>
     </div>
-    <div class="row">
-        <!--DATA TABLE CENTRO DINÁMICO-->
-        <div class="row">
-            <div class="container col offset-3 pt-1 tab-content" id="nav-tabContent">
-                <table class="tab-pane fade show table w-100 border border-3" id="list-recibidos" role="tabpanel" aria-labelledby="list-recibidos-list">
+
+
+
+    <div class="justify-content-start pt-2 ps-2">
+            <button class=" btn btn-success lateral" id="sidebarToggle"><i class="fa-solid fa-bars"></i></button>
+        </div>
+    <div id="page-content-wrapper" class="container">
+        
+        <div class="">
+            <div class="col col-12 tab-content" id="nav-tabContent">
+                <table class="tab-pane fade show table border border-3 text-center" id="list-recibidos" role="tabpanel" aria-labelledby="list-recibidos-list">
                     <thead>
                         <tr>
-                            <th scope="col">Color</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Turtle</th>
+                            <th scope="col">Prioridad Doc.</th>
+                            <th scope="col">Asunto Doc.</th>
+                            <th scope="col">Comentario Doc.</th>
+                            <th scope="col">N° de Folio.</th>
+                            <th scope="col">Estado del Doc.</th>
+                            <th scope="col">Seguimiento Doc.</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <th scope="row">Blue</th>
-                            <td>Leonardo</td>
-                            <td>da Vinci</td>
-                            <td>Leo</td>
+                            <td>Importante <i class="fa-solid fa-circle-exclamation" style="color: red;"></i></td>
+                            <th>Gestión Horas</th>
+                            <td>Gestionar Horas Extras..</td>
+                            <td>2022-001</td>
+                            <td>Pendiente</td>
+                            <td>
+                                <a href="" class="fa-regular fa-eye text-decoration-none text-dark">
+                                </a>
+                            </td>
                         </tr>
                         <tr>
-                            <th scope="row">Red</th>
-                            <td>Raffaello</td>
-                            <td>Sanzio da Urbino</td>
-                            <td>Raph</td>
+                            <td>Importante <i class="fa-solid fa-circle-exclamation" style="color: red;"></i></td>
+                            <th>Gestion Horas</th>
+                            <td>Gestionar Horas Extras..</td>
+                            <td>2022-001</td>
+                            <td>Pendiente</td>
+                            <td>
+                                <a href="" class="fa-regular fa-eye text-decoration-none text-dark">
+                                </a>
+                            </td>
                         </tr>
                         <tr>
-                            <th scope="row">Purple</th>
-                            <td>Donato</td>
-                            <td>di Niccolò di Betto Bardi</td>
-                            <td>Donnie</td>
+                            <td>Ordinario <i class="fa-solid fa-envelope"></i></td>
+                            <th>Gestion Horas</th>
+                            <td>Gestionar Horas Extras..</td>
+                            <td>2022-001</td>
+                            <td>Pendiente</td>
+                            <td>
+                                <a href="" class="fa-regular fa-eye text-decoration-none text-dark">
+                                </a>
+                            </td>
                         </tr>
                         <tr>
-                            <th scope="row">Orange</th>
-                            <td>Michelangelo</td>
-                            <td>di Lodovico Buonarroti Simoni</td>
-                            <td>Mikey</td>
+                            <td>Privado <i class="fa-solid fa-lock"></i></td>
+                            <th>Gestion Horas</th>
+                            <td>Gestionar Horas Extras..</td>
+                            <td>2022-001</td>
+                            <td>Pendiente</td>
+                            <td>
+                                <a href="" class="fa-regular fa-eye text-decoration-none text-dark">
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Importante <i class="fa-solid fa-circle-exclamation" style="color: red;"></i></td>
+                            <th>Gestion Horas</th>
+                            <td>Gestionar Horas Extras..</td>
+                            <td>2022-001</td>
+                            <td>Pendiente</td>
+                            <td>
+                                <a href="" class="fa-regular fa-eye text-decoration-none text-dark">
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Importante <i class="fa-solid fa-circle-exclamation" style="color: red;"></i></td>
+                            <th>Gestion Horas</th>
+                            <td>Gestionar Horas Extras..</td>
+                            <td>2022-001</td>
+                            <td>Pendiente</td>
+                            <td>
+                                <a href="" class="fa-regular fa-eye text-decoration-none text-dark">
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Importante <i class="fa-solid fa-circle-exclamation" style="color: red;"></i></td>
+                            <th>Gestion Horas</th>
+                            <td>Gestionar Horas Extras..</td>
+                            <td>2022-001</td>
+                            <td>Pendiente</td>
+                            <td>
+                                <a href="" class="fa-regular fa-eye text-decoration-none text-dark">
+                                </a>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
@@ -231,5 +295,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
                 </table>
             </div>
         </div>
+
     </div>
 </div>
+
+
+
+
+
+
+
+<!--DATA TABLE CENTRO DINÁMICO-->
--- application/views/template/cabecera.php
+++ application/views/template/cabecera.php
@@ -9,10 +9,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link href="<?= base_url('assets/css/style.css') ?>" rel="stylesheet" type="text/css">
+    <link href="<?= base_url('assets/css/sidebar.css') ?>" rel="stylesheet" type="text/css">
     <title>N°recibidos-usuario</title>
 </head>
 
-<body>
+<body class="bodyCustom">
     <!--BARRA DE NAVEGACIÓN -->
     <nav class="navbar custom row p-1">
         <div class="col col-2">
@@ -70,15 +71,23 @@
             </div>
         </div>
 
-        </div>
-
-
-
         <div class="col col-2 pt-2">
             <div class="text-white text-center text-uppercase fs-6">Usuario <i class="fa-regular fa-circle-user"></i></div>
             <div class="text-center"> <button class="buttonCustom text-uppercase fs-6 m-1" id="cerrarSession"> Cerrar Session</button></div>
 
         </div>
 
-        
+
     </nav>
+
+
+
+
+
+    <div class="ms-2 col col-12">
+
+
+    </div>
+    </div>
+
+    </div>
